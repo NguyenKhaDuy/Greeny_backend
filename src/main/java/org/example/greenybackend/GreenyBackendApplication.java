886266f0a928e3de108f8dcfacc0fb51469906de
NguyenKhaDuy <114864623+NguyenKhaDuy@users.noreply.github.com>
2026-06-01 22:56:43 +0700

    first commit

---
+++ src/main/java/org/example/greenybackend/GreenyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.greenybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/greenybackend/GreenyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.greenybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
